a768e5d6dba127d9713ea292fc5e8370d1b892b0
muhammadbilal55507 <154608562+muhammadbilal55507@users.noreply.github.com>
2024-02-28 18:44:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+alert()
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to JS Land...\nHappy Coding!")
+
+
+alert("Welcome to JS Land")
+
+alert("Happy Coding!")
+
+Swal.fire("Happy Coding");
+
+Swal.fire("Error! Please enter a valid password");
+
+
+Swal.fire("Welcome to JS Land");
+
+Swal.fire("Welcome to JS Land...\nHappy Coding!");
