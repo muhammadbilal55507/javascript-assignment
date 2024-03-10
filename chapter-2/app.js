f3d59b9ceb237374c40b9336dbde5ca50bb6bb14
muhammadbilal55507 <154608562+muhammadbilal55507@users.noreply.github.com>
2024-03-11 00:13:08 +0500

    Add files via upload

---
+++ chapter-2/app.js
@@ -0,0 +1,25 @@
+// var username;
+// var myName = "Bilal";
+// var message = "Hello World";
+// alert(message);
+
+// var age = 15;
+// var studying = "Web and app development";
+
+// alert(myName);
+// alert(age);
+// alert(studying);
+
+// var food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(food)
+
+// var email = "my email is muhammadbilal55507@gmail.com";
+// alert(email)
+
+// var book = "A smarter way to learn JavaScript";
+// alert(book)
+
+// document.write("Yah! I can write HTML content through Javascript");
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design)
