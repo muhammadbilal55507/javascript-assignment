9f0b2e76593a3833e5edd6494d1c394e29b9399c
muhammadbilal55507 <154608562+muhammadbilal55507@users.noreply.github.com>
2024-03-11 00:14:23 +0500

    Add files via upload

---
+++ chapter-3/app.js
@@ -0,0 +1,11 @@
+// var age = 20;
+// alert("I am "+age+" years old");
+
+// var birthYear = 2003;
+// document.write("My birthyear is "+birthYear)
+
+// var visitor = "Bilal";
+// var quantity = 5;
+// var product = " tshirts";
+
+// document.write(visitor+" ordered "+quantity+product+" on XYZ clothing store");
